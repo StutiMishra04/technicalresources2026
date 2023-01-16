9c8e9cfaff032419f5b1e79251696d96eb8bfe8f
StutiMishra04 <122737291+StutiMishra04@users.noreply.github.com>
2023-01-17 00:03:10 +0530

    Submitting answer 4 of assignment 1

---
+++ Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Numbers {
+    public static void main(String[] args){
+        int i = 1;
+        while (i <= 9){
+            System.out.println(i);
+            i = i + 1;
+        }
+
+        System.out.println("Done!!");
+    }
+    
+}
